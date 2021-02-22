8adb7fcd095e18fc9fab071b3e1f682c45d78db6
atom-young <atom.yang@aelf.io>
2021-02-22 17:43:37 +0800

    add account use inviteCodes and account count

--- src/mapping.ts
+++ src/mapping.ts
@@ -9,12 +9,16 @@ import {
 import {
   Account,
   Receipt,
-  Invitor
+  Invitor,
+  AccountCount,
+  InvitorAccountSnap
 } from "./types/schema";
 import {
   ELF_ADDRESS,
   convertTokenToDecimal,
-  ZERO_BD
+  ZERO_BD,
+  ZERO_BI,
+  ONE_BI
 } from "./helper";
 
 function isELF(address: Address): boolean {
@@ -40,17 +44,47 @@ function createInvitor(
   if (invitor === null && event.params.invitingCode !== '') {
     invitor = new Invitor(event.params.invitingCode);
     invitor.amount = ZERO_BD;
+    invitor.receiptsCount = ZERO_BI;
+    invitor.accountCount = ZERO_BI;
     invitor.save();
   }
   return invitor as Invitor;
 }
 
+function updateCount(account: Address): void {
+  let count = AccountCount.load('1');
+  if (count === null) {
+    count = new AccountCount('1');
+    count.count = ZERO_BI;
+  }
+  let user = Account.load(account.toHexString());
+  if (user === null) {
+    count.count = count.count.plus(ONE_BI);
+  }
+  count.save();
+}
+
+function createInvitorAccountSnap(invitor: Invitor, account: Account): void {
+  let id = invitor.id + '-' + account.id;
+  let snap = InvitorAccountSnap.load(id);
+  if (snap === null) {
+    snap = new InvitorAccountSnap(id);
+    snap.inviteCode = invitor.id;
+    snap.sender = account.id;
+    snap.save();
+    invitor.accountCount = invitor.accountCount.plus(ONE_BI);
+    invitor.save();
+  }
+}
+
 export function handleNewReceipt(event: NewReceipt): void {
   if (isELF(event.params.asset)) {
     let amount = convertTokenToDecimal(event.params.amount, BI_18);
+    updateCount(event.params.owner);
     let account = createAccount(event.params.owner);
     let receipt = Receipt.load(event.params.receiptId.toString());
     let invitor = createInvitor(event);
+    createInvitorAccountSnap(invitor, account);
     if (receipt === null) {
       receipt = new Receipt(event.params.receiptId.toString());
       receipt.amount = amount;
@@ -62,6 +96,7 @@ export function handleNewReceipt(event: NewReceipt): void {
     account.amount = account.amount.plus(amount);
     account.save();
     invitor.amount = invitor.amount.plus(amount);
+    invitor.receiptsCount = invitor.receiptsCount.plus(ONE_BI);
     invitor.save();
   }
 }
